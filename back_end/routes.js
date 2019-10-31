ff9a22735ec5cad83e3fedb8fae5e51358b18a54
VithuJey <vithushjeytharma@gmail.com>
2019-10-31 14:26:58 +0530

    Add routers

--- back_end/routes.js
+++ back_end/routes.js
@@ -3,7 +3,11 @@ var routes = express.Router();
 
 /** requires all classes that handles routes */
 var UserRoute = require("./user/UserRoute");
+var CourseRoute = require("./Course/CourseRoute");
+var CourseStudentRoute = require("./CourseStudent/CourseStudentRoute");
 /** routes the request to the specified class */
 routes.use("/user", UserRoute);
+routes.use("/course", CourseRoute);
+routes.use("/coursestudent", CourseStudentRoute);
 
 module.exports = routes;
