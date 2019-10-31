823302ab69e815b4c3eea59f6e330a892eb58d92
Marjan Mukram <marjan@Mahmoods-MacBook-Air.local>
2019-10-31 21:09:21 +0530

    SIgn in updated

--- front_end/src/signin.js
+++ front_end/src/signin.js
@@ -1,62 +1,105 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import axios from 'axios';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import StudentLanding from './components/StudentLanding';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+export default class SignInSide extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+        email:'',
+        password:'', 
+    }
+  }
 
-export default function SignInSide() {
-  const classes = useStyles();
+  handleLogin = (e) => {
+    e.preventDefault();
+    const email = this.state.email;
+    const password = this.state.password;
+
+    if(!email || !password){
+        alert("User name or password is empty");
+    } else {
+        window.sessionStorage.setItem('email', email);
+
+        if(email){
+            axios.get('http://localhost:4000/api/user/get/'+ email)
+                .then(resJson => {
+                    console.log(resJson)
+                    if(resJson.data[0].password === password) {
+                        // ReactDOM.render(<StudentLanding/>, document.getElementById('root'));
+                        this.setState({loginSucuess:true});
+                    } else {
+                      this.setState({loginSucuess:false});
+                        alert("You have entered an invalid password",this.state.password)
+                    }
+                })
+                .catch(err => console.log(err));
+        } else if(email.startsWith("S") || email.startsWith("s")){
+            console.log("Student");
+            this.setState({loginSucuess:true});
+        } else if(email.startsWith("I") || email.startsWith("i")){
+            axios.get('http://localhost:4000/api/instructor/getByReg/'+email)
+                .then(resJson => {
+                    if(resJson.data[0].password == password) {
+                        this.setState({loginSucuess:true});
+                    } else {
+                      this.setState({loginSucuess:true});
+                        alert("You have entered an invalid password",this.state.password)
+                    }
+                })
+                .catch(err => console.log(err))
+        } else {
+          this.setState({loginSucuess:false});
+            alert("User name is invalid")
+        }
+    }
+  }
+
+
+  useStyles = makeStyles(theme => ({
+    root: {
+      height: '100vh',
+    },
+    image: {
+      backgroundImage: 'url(https://source.unsplash.com/random)',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+    paper: {
+      margin: theme.spacing(8, 4),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+  render(){
+    const classes = this.useStyles;
 
   return (
     <Grid container component="main" className={classes.root}>
@@ -70,7 +113,7 @@ export default function SignInSide() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <form className={classes.form} noValidate>
+          {/* <form className={classes.form} noValidate> */}
             <TextField
               variant="outlined"
               margin="normal"
@@ -81,6 +124,8 @@ export default function SignInSide() {
               name="email"
               autoComplete="email"
               autoFocus
+              onChange={(e)=>{this.setState({email:e.target.value})}}
+              value={this.state.email}
             />
             <TextField
               variant="outlined"
@@ -92,13 +137,15 @@ export default function SignInSide() {
               type="password"
               id="password"
               autoComplete="current-password"
+              onChange={(e)=>{this.setState({password:e.target.value})}}
+              value={this.state.password}
             />
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             />
             <Button
-              type="submit"
+              onClick={this.handleLogin}
               fullWidth
               variant="contained"
               color="primary"
@@ -108,22 +155,15 @@ export default function SignInSide() {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
               </Grid>
             </Grid>
             <Box mt={5}>
-              {/* <Copyright /> */}
             </Box>
-          </form>
         </div>
       </Grid>
     </Grid>
   );
+}
 }
