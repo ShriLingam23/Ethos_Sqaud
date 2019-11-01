d7c9b67a27c3a5b8631ad5772d8bb2daa7d38291
Marjan Mukram <marjan@Mahmoods-MacBook-Air.local>
2019-11-01 08:42:42 +0530

    Code clean

--- front_end/src/components/Authentication/signin.js
+++ front_end/src/components/Authentication/signin.js
@@ -12,9 +12,6 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import StudentLanding from '../StudentLanding';
-import { FaWindows } from 'react-icons/fa';
-
 import Swal from 'sweetalert2'
 
   const useStyles = makeStyles(theme => ({
